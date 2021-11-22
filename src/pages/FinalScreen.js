7476af5cdb10ae02d9176c625ed77d44f967fed3
Benjamin Kandaris <benjaminkandaris@yahoo.com>
2021-11-22 13:25:15 -0500

    completed

--- src/pages/FinalScreen.js
+++ src/pages/FinalScreen.js
@@ -1,11 +1,32 @@
-import React from 'react'
+import React from 'react';
+import { Box } from '@mui/system';
+import { Typography, Button } from '@mui/material';
+import { useDispatch, useSelector } from 'react-redux';
+import { useNavigate } from 'react-router';
+import { handleScoreChange, handleAmountChange } from '../redux/actions';
 
 const FinalScreen = () => {
-    return (
-        <div>
-            Final Screen
-        </div>
-    )
-}
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const { score } = useSelector((state) => state);
+  const handleBackToSettings = () => {
+    dispatch(handleScoreChange(0));
+    dispatch(handleAmountChange(50));
+    navigate('/');
+  };
 
-export default FinalScreen
+  return (
+    <div>
+      <Box mt={30}>
+        <Typography variant='h3' fontWeight='bold' mb={3}>
+          Final Score {score}
+        </Typography>
+        <Button variant='outline' onClick={handleBackToSettings}>
+          Back to settings
+        </Button>
+      </Box>
+    </div>
+  );
+};
+
+export default FinalScreen;
